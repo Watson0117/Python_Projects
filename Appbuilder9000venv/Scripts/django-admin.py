ffccbac64c4c8937a5d786016edf9b971533499c
Joshua Watson <watson0117@gmail.com>
2021-07-16 14:54:21 -0600

    update 7/16/21

---
+++ Appbuilder9000venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!D:\Watson Tech Academy repositories\Python_Projects\Appbuilder9000venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
