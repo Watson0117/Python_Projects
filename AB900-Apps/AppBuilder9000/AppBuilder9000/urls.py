4dd85d2ccbb674570627c32bf47d6f08347dbad3
Joshua Watson <watson0117@gmail.com>
2021-07-23 13:04:59 -0600

    AppBuilder9000 update final
    7/23/21

---
+++ AB900-Apps/AppBuilder9000/AppBuilder9000/urls.py
@@ -0,0 +1,43 @@
+"""AppBuilder9000 URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+#from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+#from django.conf import settings    #FOR PILLOW LIBRARY
+#from django.conf.urls.static import static     #FOR PILLOW LIBRARY
+from django.contrib import admin
+from django.urls import path, include
+from . import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', views.home, name='home'),
+    path('SciFiWatchlist/', include('SciFiWatchlist.urls')),
+    path('HipHopReviews/', include('HipHopReviews.urls')),
+    path('Quotes/', include('Quotes.urls')),
+    path('ZooAnimals/', include('ZooAnimals.urls')),
+    path('VillagerRegistry_Index/', include('Villager_Registry.urls')),
+    path('Recipe_Maker/', include('Recipe_Maker.urls')),
+    path('RidleyVerse/', include('RidleyVerse.urls')),
+    path('WorkingComedians/', include('WorkingComedians.urls')),
+    path('Blogs/', include('Blogs.urls')),
+    path('NBAstats/', include('NBAstats.urls')),
+    path('PreciousMetals/', include('PreciousMetals.urls')),
+    path('ControlInventory/', include('ControlInventory.urls'))
+]
+
+#urlpatterns +=staticfiles_urlpatterns()
+
+#if settings.DEBUG:  # FOR PILLOW LIBRARY
+#    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
