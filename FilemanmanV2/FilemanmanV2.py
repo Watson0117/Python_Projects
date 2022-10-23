438566d335f583edefa5faedaedf39b656e816b0
Joshua Watson <watson0117@gmail.com>
2022-10-23 02:47:17 -0600

    updater

--- FilemanmanV2/FilemanmanV2.py
+++ FilemanmanV2/FilemanmanV2.py
@@ -1,12 +1,11 @@
 import os
 import re
 from re import sub
-from tkinter import Tk
+#from tkinter import Tk
 from tkinter.filedialog import Directory, askdirectory
 print("The first step will be to chooses the folder containing the files you wish to rename.\n")
-print("After this selection is made the user may pick up to 3 keywords to remove from the file names. leave unneeded inputs blank and continue.\n")
+print("After this selection is made the user may pick up to 3 keywords to remove from the file names your selections are case sensitive. leave unused inputs blank and continue.\n")
 dirName = askdirectory() # shows dialog box and return the path
-print(dirName)
 
 ###########---kEYWORD KEYS---######################
 keyword1 = input('Choose the first keyword would you like to remove from file names?\n')# First keyword
@@ -17,16 +16,17 @@ keyword3 = input('Choose the third keyword would you like to remove from file na
 keysearch = [keyword1, keyword2, keyword3]
 badformating = ["\.", "-", "_", "  "]
 keywords = ["720", "720p", "480", "480p","1080p", "1080", "DUAL Audio", "10bit", "DVD", "BluRay", "BRrip", "x256", "HDrip", "EVO"]
+emStr = ""# initialize an empty string
 
 ##################### Main ############################################################
 for fname in os.listdir(dirName):
-    if keyword1 or keyword2 or keyword3 in fname:
-        print(fname, "has one of the keywords\n")
+    if emStr.join(keysearch) in fname:
+        print(fname, "has one of the keywords with keysearch\n")
 
 ################### Keywords ##########################################################
 for filename in os.listdir(dirName):
-    str1 = ""# initialize an empty string
-    fName = (str1.join(filename)) # changes list to string
+    #emStr = ""# initialize an empty string
+    fName = (emStr.join(filename)) # changes list to string
     curfilename, suff = os.path.splitext(fName) # splits the path into root and extension pair such that root + extension == path
     upfilename = sub('|'.join(keysearch),'',curfilename) # finds any instances of keysearch and repalaces them with an empty string.
     finfilename = (upfilename + suff)# remakes the full file name
@@ -36,16 +36,16 @@ for filename in os.listdir(dirName):
 
 ################  Cleanup --- () and [] removal ----###########################################################
 for filename in os.listdir(dirName):
-    str1 = ""# initialize an empty string
-    fName = (str1.join(filename)) # changes list to string
+    #emStr = ""# initialize an empty string
+    fName = (emStr.join(filename)) # changes list to string
     newFname = re.sub(r'\[.*?\]',"",fName) # this finds all instances of [] deletes them and the contents.
     filepath = os.path.join(dirName, filename) # should take the path and file name an create a string
     newfilepath = os.path.join(dirName, filename.replace(filename,newFname)) # should creat the string to rename the file, take dir name and file name but replace fiel name with newFname
     os.rename(filepath, newfilepath)# this should take the now modified file name and renamt the target with the new name.
 
 for filename in os.listdir(dirName):
-    str1 = ""# initialize an empty string
-    fName = (str1.join(filename)) # changes list to string
+    emStr = ""# initialize an empty string
+    fName = (emStr.join(filename)) # changes list to string
     newFname = re.sub(r'\(.*?\)',"",fName)# this finds all instances of () deletes them and the contents
     filepath = os.path.join(dirName, filename) # should take the path and file name an create a string
     newfilepath = os.path.join(dirName, filename.replace(filename,newFname)) # should creat the string to rename the file, take dir name and file name but replace fiel name with newFname
@@ -58,8 +58,8 @@ for filename in os.listdir(dirName):
 
 #######################   formating    ###########################
 for filename in os.listdir(dirName):
-    str1 = ""# initialize an empty string
-    fName = (str1.join(filename)) # changes list to string
+    emStr = ""# initialize an empty string
+    fName = (emStr.join(filename)) # changes list to string
     curfilename, suff = os.path.splitext(fName) # splits the path into root and extension pair such that root + extension == path
     upfilename = sub('|'.join(badformating),' ',curfilename)
     finfilename = (upfilename + suff)# recombines the name and suff into single string
@@ -68,8 +68,8 @@ for filename in os.listdir(dirName):
     os.rename(filepath, newfilepath)#renames file with newfilename
 
 for filename in os.listdir(dirName):
-    str1 = ""# initialize an empty string
-    fName = (str1.join(filename)) # changes list to string
+    emStr = ""# initialize an empty string
+    fName = (emStr.join(filename)) # changes list to string
     curfilename, suff = os.path.splitext(fName) # splits the path into root and extension pair such that root + extension == path
     upfilename = curfilename.rstrip(" ") # rstrip removes whitspace at the end of a string
     finfilename = (upfilename + suff) # recombine them
